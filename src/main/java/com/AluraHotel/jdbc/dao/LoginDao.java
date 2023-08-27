83e9ed4545addc740867881455b6b08bd92b8fde
Andres Charry <ingeniero.andres.charry@gmail.com>
2023-08-27 12:40:22 -0500

    dao logic was added

---
+++ src/main/java/com/AluraHotel/jdbc/dao/LoginDao.java
@@ -0,0 +1,47 @@
+package com.AluraHotel.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.AluraHotel.jdbc.model.User;
+
+public class LoginDao {
+	//attributes
+	final private Connection con;
+	
+	public LoginDao(Connection con) {
+		this.con = con;
+	}
+	
+	public List<User> ConfirmUser(String userName, String password) {
+		List<User> resultado = new ArrayList<>();
+		
+		try {
+			final PreparedStatement statement = con.prepareStatement("SELECT user, password "
+					+ "FROM Users "
+					+ "WHERE user = ? AND password = ?");
+			try(statement){
+				statement.setString(1, userName);
+				statement.setString(2, password);
+				
+				statement.execute();
+				
+				ResultSet resultSet = statement.getResultSet();
+				while(resultSet.next()) {
+					User user = new User(
+							resultSet.getString("user"),
+							resultSet.getString("password"));
+					resultado.add(user);
+				}
+				return resultado;
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
